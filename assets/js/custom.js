36e71f44b58cae70ee546cd57f6b3f319e648091
William Liska <william.liska@gmail.com>
2018-08-29 20:44:12 -0400

    More customization

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,12 @@
  *  Place here all your custom js. Make sure it's loaded after app.js
  *
  * ---------------------------------------------------------------------------- */
+ $('.datatable-basic').DataTable({
+ 	autoWidth: false,
+ 	dom: '<"datatable-header"fl><"datatable-scroll"t><"datatable-footer"ip>',
+     language: {
+         search: '<span>Filter:</span> _INPUT_',
+         lengthMenu: '<span>Show:</span> _MENU_',
+         paginate: { 'first': 'First', 'last': 'Last', 'next': '→', 'previous': '←' }
+     }
+ });
