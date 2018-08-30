d9d3955662b3e6f306e9b037581e9da65d26f67e
William Liska <william.liska@gmail.com>
2018-08-29 21:02:04 -0400

    Added global_assets and modified gulpfile to make customizations easier.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,6 +5,13 @@
  *  Place here all your custom js. Make sure it's loaded after app.js
  *
  * ---------------------------------------------------------------------------- */
+ // Setup module
+ // ------------------------------
+
+ var H1BPrototype = function() {
+
+   var _setupDatatable = function() {
+
  $('.datatable-basic').DataTable({
  	autoWidth: false,
  	dom: '<"datatable-header"fl><"datatable-scroll"t><"datatable-footer"ip>',
@@ -13,4 +20,23 @@
          lengthMenu: '<span>Show:</span> _MENU_',
          paginate: { 'first': 'First', 'last': 'Last', 'next': '→', 'previous': '←' }
      }
+ });
+};
+
+ //
+     // Return objects assigned to module
+     //
+
+     return {
+         init: function() {
+             _setupDatatable();
+         }
+     }
+
+}();
+ // Initialize module
+ // ------------------------------
+
+ document.addEventListener('DOMContentLoaded', function() {
+     H1BPrototype.init();
  });
